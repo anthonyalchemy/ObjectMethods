1c72520493113922cb5bfee45e0614f4357cac5f
anthonyalchemy <anthony@alchemytechs.com>
2019-07-31 14:59:38 +0100

    Add project files.

---
+++ ObjectMethods/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectMethods
+{
+    class Student
+    {
+
+        public string name;
+        public string major;
+        public double gpa;
+
+        public Student (string aName, string aMajor, double aGpa)//constructor
+        {
+            name = aName;
+            major = aMajor;
+            gpa = aGpa;
+        }
+
+        //creating object method below 
+
+        public bool HasHonors()
+        {
+            if (gpa >= 3.5)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
